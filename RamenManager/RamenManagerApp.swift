28d24060255bf3d4ecfa047ebc3ee68024d639c9
Soham Gupta <guptasoham@outlook.com>
2021-12-13 19:11:20 -0600

    Initial Commit

---
+++ RamenManager/RamenManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamenManagerApp.swift
+//  RamenManager
+//
+//  Created by soham gupta on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RamenManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
